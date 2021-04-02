06751bf2e508e4dd4624a9fb7881baf77d4427d7
Gowthamhm <gowthamhm001@gmail.com>
2021-04-02 12:19:00 +0530

    serialization error

--- src/main/java/com/event/qrcode/bean/QrCodeBean.java
+++ src/main/java/com/event/qrcode/bean/QrCodeBean.java
@@ -1,4 +1,74 @@
 package com.event.qrcode.bean;
 public class QrCodeBean {
-	
+	 private String foldername;
+	    private String orignal_text;
+	    private String qouted_Text;
+	    private String phone;
+	    private String Directory_path;
+	    private String inQrcodeFileName;
+	    private String outQrcodeFileName;
+	    private String inQrcodeText;
+	    private String outQrcodeText;
+	    private int status;
+	    
+		public String getFoldername() {
+			return foldername;
+		}
+		public void setFoldername(String foldername) {
+			this.foldername = foldername;
+		}
+		public String getOrignal_text() {
+			return orignal_text;
+		}
+		public void setOrignal_text(String orignal_text) {
+			this.orignal_text = orignal_text;
+		}
+		public String getQouted_Text() {
+			return qouted_Text;
+		}
+		public void setQouted_Text(String qouted_Text) {
+			this.qouted_Text = qouted_Text;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		public String getDirectory_path() {
+			return Directory_path;
+		}
+		public void setDirectory_path(String directory_path) {
+			Directory_path = directory_path;
+		}
+		public String getInQrcodeFileName() {
+			return inQrcodeFileName;
+		}
+		public void setInQrcodeFileName(String inQrcodeFileName) {
+			this.inQrcodeFileName = inQrcodeFileName;
+		}
+		public String getOutQrcodeFileName() {
+			return outQrcodeFileName;
+		}
+		public void setOutQrcodeFileName(String outQrcodeFileName) {
+			this.outQrcodeFileName = outQrcodeFileName;
+		}
+		public String getInQrcodeText() {
+			return inQrcodeText;
+		}
+		public void setInQrcodeText(String inQrcodeText) {
+			this.inQrcodeText = inQrcodeText;
+		}
+		public String getOutQrcodeText() {
+			return outQrcodeText;
+		}
+		public void setOutQrcodeText(String outQrcodeText) {
+			this.outQrcodeText = outQrcodeText;
+		}
+		public int getStatus() {
+			return status;
+		}
+		public void setStatus(int status) {
+			this.status = status;
+		}
 }
